34739b288dfb684394794461b5ee68756af24b06
sansy98 <60301258+sansy98@users.noreply.github.com>
2020-10-06 20:55:47 +0200

    This was a f****ng mess but now it works.

--- PythonCode/pwd_gen.py
+++ PythonCode/pwd_gen.py
@@ -4,29 +4,30 @@ from functools import partial
 from operator import methodcaller
 
 
-def call_n_times(function):
+def call_n_times(tim):
     clock = 0
     got = ""
-    while clock != usnput:
-        clock = clock + 1
-        got = got + function
+    while (clock - tim) != 0:
+        form = [str(random.randint(0, 9)), random.choice(abc).upper(), random.choice(abc)]
+        clock += 1
+        got += random.choice(form)
     else:
         return got
 
 
 abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-form = [str(random.randint(0, 9)), random.choice(abc).upper(), random.choice(abc)]
+isEnd = False
 
-
-usnput = input("How many characters long do you want your password to be? : ")
-
-while usnput.isdigit():
-    if int(usnput) >= 5 <= 20:
-        pwd = call_n_times(random.choice(form))
-        print("Here is your password ---->  " + pwd)
-    else:
-        print("Sorry! The password must be between 5 and 20 characters long")
-else:
-    print("Please, enter a number")
-    continue
-
+while True and not isEnd:
+    usnput = input("How many characters long do you want your password to be? : ")
+    while usnput.isdigit():
+        if int(usnput) > 4 and int(usnput) < 21:
+            pwd = call_n_times(int(usnput))
+            print("Here is your password ---->  " + pwd)
+            isEnd = True
+            break
+        else:
+            print("Sorry! The password must be between 5 and 20 characters long")
+            break
+    if not usnput.isdigit() and not isEnd:
+        print("Please, enter a number")
